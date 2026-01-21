27430c021aa531d907023bd97ad0318f4b9eca65
Yasser0618 <yas.iraola@gmail.com>
2026-01-21 12:18:39 +0100

    Añadimos la ruta generica que crea las 7 rutas de forma interna con el comando y la ruta añadida de confirmDestroy. Para asegurarse de las rutas creadas en terminal 'php artisan route:list'

--- libreriapasapagina/routes/web.php
+++ libreriapasapagina/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibroController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//De forma interna laravel crea una serie de rutas, en concreto 7, las que nos crea en el controlador
+Route::resource('libro', LibroController::class);
+
+//El metodo confrimDestroy hay que ponerlo por separado porque no te o crea por defecto
+Route::get('/libro/{libro}/confirm', [LibroController::class, 'confirmDestroy'])->name('libro.confirmDestroy');
